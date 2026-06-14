89117829410847002254b8a5b9c59bf5a7f043c9
marcosdeivide <marcosdeivide06@gmail.com>
2026-06-14 03:58:33 -0300

    dashboard v2 contador de producao

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
-const body = document.querySelector("body")
-const botaoTema = document.querySelector("#bolinha")
+const body = document.querySelector("body");
+const botaoTema = document.querySelector("#btn-tema");
+const producaoElemento = document.querySelector("#producao");
+const botaomais = document.querySelector("#mais");
+const botaomenos = document.querySelector("#menos");
 
+let producao = 2;
+let clique = 0;
 
 botaoTema.addEventListener("click", function () {
- body.classList.toggle("dark")
- console.log(body.classList.contains("dark"))
- botaoTema.textContent = "ola marcos"
-})
+  body.classList.toggle("dark");
+  console.log(body.classList.contains("dark"));
+
+  clique = clique + 1;
+  console.log(clique);
+
+  if (body.classList.contains("dark")) {
+    botaoTema.textContent = "☀️ Modo Claro";
+  } else {
+    botaoTema.textContent = "🌙 Modo Escuro";
+  }
+});
+botaomais.addEventListener("click", function () {
+   producao = producao + 1 ;
+   console.log(producao);
+
+   producaoElemento.textContent = producao + "un"
+
+});
+botaomenos.addEventListener("click", function () {
+   if (producao > 0){
+    producao = producao - 1;
+    
+   console.log(producao);
+
+   producaoElemento.textContent = producao + "un"
+    
+   }
+   
+
+  
+
+});
